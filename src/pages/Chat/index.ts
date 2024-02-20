65e52f5f81bb8523d19bb0fee6db9743e59ce103
sergrage <elenergetica87@mail.ru>
2024-02-20 20:51:06 +0300

    fix img

--- src/pages/Chat/index.ts
+++ src/pages/Chat/index.ts
@@ -137,7 +137,7 @@ const chatMessages: ChatMessagesInterface[] = [
                 },
                 {
                     companion: true,
-                    img: '../../../static/img/blacksabbath.jpeg',
+                    img: '/static/img/blacksabbath.jpeg',
                     alt: 'blacksabbath',
                     time: '15:13',
                 },
@@ -180,7 +180,7 @@ const chatMessages: ChatMessagesInterface[] = [
                 },
                 {
                     my: true,
-                    img: '../../../static/img/savatage.jpg',
+                    img: '/static/img/savatage.jpg',
                     alt: 'savatage',
                     time: '15:13',
                 },
@@ -207,7 +207,7 @@ const chatMessages: ChatMessagesInterface[] = [
                 },
                 {
                     companion: true,
-                    img: '../../../static/img/blacksabbath.jpeg',
+                    img: '/static/img/blacksabbath.jpeg',
                     alt: 'blacksabbath',
                     time: '15:13',
                 },
@@ -250,7 +250,7 @@ const chatMessages: ChatMessagesInterface[] = [
                 },
                 {
                     my: true,
-                    img: '../../../static/img/savatage.jpg',
+                    img: '/static/img/savatage.jpg',
                     alt: 'savatage',
                     time: '15:13',
                 },
