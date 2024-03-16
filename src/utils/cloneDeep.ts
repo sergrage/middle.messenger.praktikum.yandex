044ab4e7f4fb7190806827cf0fd4c2b307ac7aae
sergrage <elenergetica87@mail.ru>
2024-03-16 17:05:51 +0300

    fix6

--- src/utils/cloneDeep.ts
+++ src/utils/cloneDeep.ts
@@ -1,71 +1,71 @@
 function cloneDeep(obj: Record<string, unknown | any>): Record<string, unknown | any> {
-    return (function _cloneDeep(item: any): Record<string, unknown | any> {
-        // Handle:
-        // * null
-        // * undefined
-        // * boolean
-        // * number
-        // * string
-        // * symbol
-        // * function
-        if (item === null || typeof item !== 'object') {
-            return item;
-        }
+  return (function _cloneDeep(item: any): Record<string, unknown | any> {
+    // Handle:
+    // * null
+    // * undefined
+    // * boolean
+    // * number
+    // * string
+    // * symbol
+    // * function
+    if (item === null || typeof item !== 'object') {
+      return item;
+    }
 
-        // Handle:
-        // * Date
-        if (item instanceof Date) {
-            return new Date(item.valueOf());
-        }
+    // Handle:
+    // * Date
+    if (item instanceof Date) {
+      return new Date(item.valueOf());
+    }
 
-        // Handle:
-        // * Array
-        if (item instanceof Array) {
-            const copy: any = [];
+    // Handle:
+    // * Array
+    if (item instanceof Array) {
+      const copy: any = [];
 
-            item.forEach((_, i) => (copy[i] = _cloneDeep(item[i])));
+      item.forEach((_, i) => (copy[i] = _cloneDeep(item[i])));
 
-            return copy;
-        }
+      return copy;
+    }
 
-        // Handle:
-        // * Set
-        if (item instanceof Set) {
-            const copy = new Set();
+    // Handle:
+    // * Set
+    if (item instanceof Set) {
+      const copy = new Set();
 
-            item.forEach((v) => copy.add(_cloneDeep(v)));
+      item.forEach((v) => copy.add(_cloneDeep(v)));
 
-            return copy;
-        }
+      return copy;
+    }
 
-        // Handle:
-        // * Map
-        if (item instanceof Map) {
-            const copy = new Map();
+    // Handle:
+    // * Map
+    if (item instanceof Map) {
+      const copy = new Map();
 
-            item.forEach((v, k) => copy.set(k, _cloneDeep(v)));
+      item.forEach((v, k) => copy.set(k, _cloneDeep(v)));
 
-            return copy;
-        }
+      return copy;
+    }
 
-        // Handle:
-        // * Object
-        if (item instanceof Object) {
-            const copy: any = {};
+    // Handle:
+    // * Object
+    if (item instanceof Object) {
+      const copy: any = {};
 
-            // Handle:
-            // * Object.symbol
-            Object.getOwnPropertySymbols(item).forEach((s) => (copy[s] = _cloneDeep(item[s])));
+      // Handle:
+      // * Object.symbol
+      Object.getOwnPropertySymbols(item).forEach((s) => (copy[s] = _cloneDeep(item[s])));
 
-            // Handle:
-            // * Object.name (other)
-            Object.keys(item).forEach((k) => (copy[k] = _cloneDeep(item[k])));
+      // Handle:
+      // * Object.name (other)
+      Object.keys(item).forEach((k) => (copy[k] = _cloneDeep(item[k])));
 
-            return copy;
-        }
+      return copy;
+    }
 
-        throw new Error(`Unable to copy object: ${item}`);
-    }(obj));
+    throw new Error(`Unable to copy object: ${item}`);
+  }(obj));
 }
 
 export default cloneDeep;
